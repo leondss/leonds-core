921023db48a97c41e1310e8d03c39226b37e1d1d
Leon <leonds@qq.com>
2019-09-19 15:48:09 +0800

    add Copyright

--- src/main/java/com/leonds/core/QueryParams.java
+++ src/main/java/com/leonds/core/QueryParams.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 leonds@qq.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.leonds.core;
 
 
@@ -13,7 +29,7 @@ public class QueryParams {
     private String q;
 
     public PageRequest pageRequest() {
-        int p = page <= 0 ? 0 : page;
+        int p = Math.max(page, 0);
         int s = size <= 0 ? 10 : size;
         return PageRequest.of(p, s, orderBy);
     }

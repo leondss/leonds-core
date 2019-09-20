7d30376390820f3f02d3e3dbbff825f682629d6b
Leon <leonds@qq.com>
2019-09-20 17:02:32 +0800

    update Copyright

--- src/main/java/com/leonds/core/Enumeration.java
+++ src/main/java/com/leonds/core/Enumeration.java
@@ -14,31 +14,8 @@
  * limitations under the License.
  */
 
-package com.leonds.core.orm;
+package com.leonds.core;
 
-/**
- * @author Leon
- */
-public enum Operator {
-    AND,
-    OR,
-    NOT,
-    LT,
-    GT,
-    EQ,
-    NEQ,
-    NONE,
-    LE,
-    GE,
-    IS_NULL,
-    IS_NOT_NULL,
-    LIKE,
-    CONTAINS,
-    STARTS_WITH,
-    ENDS_WITH,
-    IN,
-    NOT_IN,
-    NEAR,
-    WITHIN,
-    ALL
+public interface Enumeration {
+    int code();
 }
